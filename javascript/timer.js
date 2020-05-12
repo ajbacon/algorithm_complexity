c638f58f6a36c189cdc8c91c2e72ba0a5133a617
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-12 15:54:31 +0100

    docs: add time unit to chart. feat: update js timer

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,6 +1,12 @@
+const { performance } = require('perf_hooks');
+
 const getRunTime = (arr, callback) => {
   'use strict';
+  const t0 = performance.now();
   console.log(callback(arr));
+  const t1 = performance.now();
+
+  console.log((t1 - t0) / 1000);
 };
 
 module.exports = getRunTime;
