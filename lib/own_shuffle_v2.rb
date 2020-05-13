126a4836219f5f0e283f7dc0d4e8804694c0d73e
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-13 09:09:22 +0100

    refactor: minor refactoring to add print output method

--- lib/own_shuffle_v2.rb
+++ lib/own_shuffle_v2.rb
@@ -13,12 +13,22 @@ def shuffle_own_v2(input)
   input
 end
 
-def runTimer()
-  (50000..1000000).step(50000) do |n| 
-    arr = (1..n).to_a
-    Timer.get_time(arr, :shuffle_own_v2)
-  end
-  
+def print_output(arr)
+  arr.each { |elm| p elm }
 end
 
-runTimer()
+def runTimer(inResolution = 50000, inMax = 1500000)
+  resolution = inResolution
+  max = inMax
+  output_arr = []
+
+  (resolution..max).step(resolution) do |n| 
+    arr = (1..n).to_a
+    output_arr.push(Timer.get_time(arr, :shuffle_own_v2))
+  end
+
+  print_output(output_arr)
+end
+
+
+runTimer(50000,1000000)
--- lib/ruby_shuffle.rb
+++ lib/ruby_shuffle.rb
@@ -4,12 +4,22 @@ def shuffle_ruby(input)
   input.shuffle()
 end
 
-def runTimer()
-  (50000..1000000).step(50000) do |n| 
-    arr = (1..n).to_a
-    Timer.get_time(arr, :shuffle_ruby)
-  end
-  
+def print_output(arr)
+  arr.each { |elm| p elm }
 end
 
-runTimer()
+def runTimer(inResolution = 50000, inMax = 1500000)
+  resolution = inResolution
+  max = inMax
+  output_arr = []
+
+  (resolution..max).step(resolution) do |n| 
+    arr = (1..n).to_a
+    output_arr.push(Timer.get_time(arr, :shuffle_ruby))
+  end
+
+  print_output(output_arr)
+end
+
+
+runTimer(50000,1000000)
