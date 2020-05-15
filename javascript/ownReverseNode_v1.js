73e73813ef35ac57735ac55e05f50b06831b6bce
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-15 14:53:28 +0100

    fix: return value. force 'use_strict'

--- javascript/ownReverseNode_v1.js
+++ javascript/ownReverseNode_v1.js
@@ -1,9 +1,10 @@
+'use strict';
 const getRunTime = require('./timerNode');
 
 const ownReverse = (arr) => {
-  output = arr;
-  arrLength = arr.length;
-  midArr = Math.floor(arrLength);
+  let output = arr;
+  let arrLength = arr.length;
+  let midArr = Math.floor(arrLength);
 
   for (let i = 0; i < midArr; i++) {
     let firstElm = i;
@@ -11,15 +12,14 @@ const ownReverse = (arr) => {
 
     [arr[firstElm], arr[lastElm]] = [arr[lastElm], arr[firstElm]];
   }
-
-  return arr.reverse();
+  return output;
 };
 
 const runTime = (inResolution = 50000, inMax = 1000000) => {
   const resolution = inResolution;
   const max = inMax;
   for (let elms = resolution; elms <= max; elms += resolution) {
-    arr = Array.from(new Array(elms), (x, i) => i + 1);
+    let arr = Array.from(new Array(elms), (x, i) => i + 1);
     getRunTime(arr, ownReverse);
   }
 };
