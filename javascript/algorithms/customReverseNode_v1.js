06c6fc3f6f970b5483a2a5f438586e84772c064b
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-17 12:56:07 +0100

    chore: add customReverse for timing

--- javascript/algorithms/customReverseNode_v1.js
+++ javascript/algorithms/customReverseNode_v1.js
@@ -1,7 +1,6 @@
 'use strict';
-const getRunTime = require('../timerNode');
 
-const ownReverse = (arr) => {
+const customReverse1 = (arr) => {
   let output = arr;
   let arrLength = arr.length;
   let midArr = Math.floor(arrLength);
@@ -15,13 +14,4 @@ const ownReverse = (arr) => {
   return output;
 };
 
-const runTime = (inResolution = 50000, inMax = 1000000) => {
-  const resolution = inResolution;
-  const max = inMax;
-  for (let elms = resolution; elms <= max; elms += resolution) {
-    let arr = Array.from(new Array(elms), (x, i) => i + 1);
-    getRunTime(arr, ownReverse);
-  }
-};
-
-runTime(5000, 200000);
+module.exports = customReverse1;
--- javascript/reversePerformance.js
+++ javascript/reversePerformance.js
@@ -1,8 +1,17 @@
+'use strict';
 // Time JavaScript native reverse function against my custom version
 
-const jsNativeReverse = require('./algorithms/jsNativeReverse');
 const shuffledArrayTimer = require('./helper_methods/shuffledArrayTimer');
+const jsNativeReverse = require('./algorithms/jsNativeReverse');
+const customReverse1 = require('./algorithms/customReverseNode_v1');
 
-timerOptions = { iterations: 35, trimPercentage: 0.2 };
+// specify timing parameters
+const timerOptions = { iterations: 35, trimPercentage: 0.2 };
+const interval = 5000;
+const maxSize = 100000;
 
-shuffledArrayTimer(jsNativeReverse, 5000, 100000, timerOptions);
+// time native function
+shuffledArrayTimer(jsNativeReverse, interval, maxSize, timerOptions);
+
+// time custom function
+shuffledArrayTimer(customReverse1, interval, maxSize, timerOptions);
