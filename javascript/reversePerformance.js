8a6116203e57439da79236889b0c6410b489a8a9
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-17 09:51:46 +0100

    refactor: allow for variable running parameters, selected and modified by an options object

--- javascript/reversePerformance.js
+++ javascript/reversePerformance.js
@@ -1,4 +1,19 @@
 const jsNativeReverse = require('./algorithms/jsNativeReverse');
-arr = [1, 2, 3];
+const timer = require('./timing/timerNode');
 
-console.log(jsNativeReverse(arr));
+timerOptions = { iterations: 35, trimPercentage: 0.2 };
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+timer(arr, jsNativeReverse, timerOptions);
+
+// const runTime = (inResolution = 50000, inMax = 1000000) => {
+//   const resolution = inResolution;
+//   const max = inMax;
+//   for (let elms = resolution; elms <= max; elms += resolution) {
+//     let arr = Array.from(new Array(elms), (x, i) => i + 1);
+//     timer(arr, jsReverse);
+//   }
+// };
+
+// runTime();
+// runTime(5000, 100000);
--- javascript/timing/timerNode.js
+++ javascript/timing/timerNode.js
@@ -1,29 +1,38 @@
+'use strict';
 const { performance } = require('perf_hooks');
 
-const getRunTime = (arr, callback) => {
-  'use strict';
-  const n = 35;
-  const trim = Math.floor(n * 0.2);
-  let timeArr = [];
-  for (let i = 0; i < n; i++) {
-    const t0 = performance.now();
-    callback(arr);
-    const t1 = performance.now();
-    // throw away first 5 results
-    if (i > 4) {
-      timeArr.push(t1 - t0);
-    }
-  }
-  const timeArr = trimOutliers(timeArr);
+const getRunTime = (arr, fnc, options = {}) => {
+  const n = options.iterations || 10;
+  const trimPercentage = options.trimPercentage || 0.1;
+
+  let timeArr = runTimer(arr, fnc, n);
+  timeArr = trimOutliers(timeArr, n, trimPercentage);
+
   const avg = average(timeArr);
   console.log(avg);
 };
 
-const trimOutliers = (timeArr) => {
+const runTimer = (arr, fnc, n) => {
+  let timeArr = [];
+  for (let i = 0; i < n; i++) {
+    const t0 = performance.now();
+    fnc(arr);
+    const t1 = performance.now();
+    // first runs may be unstable, throw away first 5 results
+    if (i > 4) {
+      timeArr.push(t1 - t0);
+    }
+  }
+  return timeArr;
+};
+
+const trimOutliers = (timeArr, n, trimPercentage) => {
+  const trim = Math.floor(n * trimPercentage);
   timeArr = timeArr.sort((a, b) => {
     return a - b;
   });
   timeArr = timeArr.slice(trim, timeArr.length - trim);
+  return timeArr;
 };
 
 const average = (timeArr) => {
