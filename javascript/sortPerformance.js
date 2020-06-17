429e002bb994fc32f409854d52f1335df833aca2
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-17 17:04:51 +0100

    feat: compare npm 'array-sort' module

--- javascript/sortPerformance.js
+++ javascript/sortPerformance.js
@@ -4,6 +4,7 @@
 const shuffledArrayTimer = require('./helper_methods/shuffledArrayTimer');
 const jsNativeSort = require('./algorithms/jsNativeSort');
 const customQuickSort = require('./algorithms/customQuickSort');
+const arraySort = require('array-sort');
 
 // specify timing parameters
 const timerOptions = { iterations: 35, trimPercentage: 0.2 };
@@ -15,6 +16,10 @@ const runTimers = () => {
   // time native function
   outFile = 'jsNativeSort.csv';
   shuffledArrayTimer(jsNativeSort, outFile, interval, maxSize, timerOptions);
+
+  // time npm array-sort module
+  outFile = 'arraySortModule.csv';
+  shuffledArrayTimer(arraySort, outFile, interval, maxSize, timerOptions);
 
   // time custom quicksort function
   outFile = 'customQuickSort.csv';
