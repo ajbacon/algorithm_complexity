63594e863aadf32af866a216c19d3fce654e15ce
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-17 15:47:28 +0100

    chore: update filename and method name for custom quicksort

--- javascript/algorithms/customQuickSort.js
+++ javascript/algorithms/customQuickSort.js
@@ -1,7 +1,6 @@
 'use strict';
-const getRunTime = require('../timerNode');
 
-const ownSort = (arr) => {
+const customQuickSort = (arr) => {
   function swap(arr, leftIndex, rightIndex) {
     var temp = arr[leftIndex];
     arr[leftIndex] = arr[rightIndex];
@@ -43,15 +42,4 @@ const ownSort = (arr) => {
   return quickSort(arr);
 };
 
-const runTime = (inResolution = 50000, inMax = 1000000) => {
-  const resolution = inResolution;
-  const max = inMax;
-  for (let elms = resolution; elms <= max; elms += resolution) {
-    let arr = Array.from(new Array(elms), (x, i) => i + 1);
-    getRunTime(arr, ownSort);
-  }
-};
-
-runTime(5000, 200000);
-
-// console.log(ownSort([4, 3, 2, 1]));
+module.exports = customQuickSort;
